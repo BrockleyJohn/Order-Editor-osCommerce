214d8eaed7382ea6284a5494f6985267c210fd4a
Juanma <mazinfriki@gmail.com>
2016-04-05 16:15:41 +0200

    r

--- admin/order_editor/11.php
+++ admin/order_editor/11.php
@@ -16,40 +16,43 @@
                                             FROM " . TABLE_ORDERS_STATUS_HISTORY . "
                                             WHERE orders_id = '" . $oID . "'
                                             ORDER BY date_added");
-if (tep_db_num_rows($orders_history_query)) {
-  while ($orders_history = tep_db_fetch_array($orders_history_query)) {
-
-    $r++;
-        $rowClass = ((($r/2) == (floor($r/2))) ? 'dataTableRowOver' : 'dataTableRow');
-
-       echo '  <tr class="' . $rowClass . '" id="commentRow' . $orders_history['orders_status_history_id'] . '" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this, \'' . $rowClass . '\')">' . "\n" .
-         '    <td class="smallText" align="center"><div id="do_not_delete"><input name="update_comments[' . $orders_history['orders_status_history_id'] . '][delete]" type="checkbox" onClick="updateCommentsField(\'delete\', \'' . $orders_history['orders_status_history_id'] . '\', this.checked, \'\', this)"></div></td>' . "\n" .
-     '    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>' . "\n" .
-         '    <td class="smallText" align="center">' . tep_datetime_short($orders_history['date_added']) . '</td>' . "\n" .
-         '    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>' . "\n" .
-         '    <td class="smallText" align="center">';
+      if (tep_db_num_rows($orders_history_query)) {
+        while ($orders_history = tep_db_fetch_array($orders_history_query)) {
+          $r++;
+          $rowClass = ((($r/2) == (floor($r/2))) ? 'dataTableRowOver' : 'dataTableRow');
+?>
+  <tr class="<?= $rowClass ?>" id="commentRow<?= $orders_history['orders_status_history_id'] ?>" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this, \'<?= $rowClass ?>\')">
+    <td class="smallText" align="center"><div id="do_not_delete"><input name="update_comments[<?= $orders_history['orders_status_history_id'] ?>][delete]" type="checkbox" onClick="updateCommentsField(\'delete\', \'<?= $orders_history['orders_status_history_id'] ?>\', this.checked, \'\', this)"></div></td>
+    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>
+    <td class="smallText" align="center"><?= tep_datetime_short($orders_history['date_added']) ?></td>
+    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>
+    <td class="smallText" align="center">
+<?php
     if ($orders_history['customer_notified'] == '1') {
-      echo tep_image(DIR_WS_ICONS . 'tick.gif', ICON_TICK) . "</td>\n";
+      echo tep_image(DIR_WS_ICONS ?>tick.gif', ICON_TICK) . </td>
     } else {
-      echo tep_image(DIR_WS_ICONS . 'cross.gif', ICON_CROSS) . "</td>\n";
+      echo tep_image(DIR_WS_ICONS ?>cross.gif', ICON_CROSS) . </td>
     }
-    echo '    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>' . "\n" .
-         '    <td class="smallText" align="left">' . $orders_status_array[$orders_history['orders_status_id']] . '</td>' . "\n";
-    echo '    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>' . "\n" .
-         '    <td class="smallText" align="left">' .
+?>
+    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>
+    <td class="smallText" align="left"><?= $orders_status_array[$orders_history['orders_status_id']] ?></td>
+    <td class="dataTableHeadingContent" align="left" width="10">&nbsp;</td>
+    <td class="smallText" align="left">
 
   tep_draw_textarea_field("update_comments[" . $orders_history['orders_status_history_id'] . "][comments]", "soft", "40", "5",
-  "" .  tep_db_output($orders_history['comments']) . "", "onChange=\"updateCommentsField('update', '" . $orders_history['orders_status_history_id'] . "', 'false', encodeURIComponent(this.value))\"") . '' . "\n" .
+  "" .  tep_db_output($orders_history['comments']) . "", "onChange=\"updateCommentsField('update', '" . $orders_history['orders_status_history_id'] . "', 'false', encodeURIComponent(this.value))\"") ?>
 
-     '    </td>' . "\n";
+     '    </td><?= "
 
-    echo '  </tr>' . "\n";
+    echo '  </tr><?= "
 
       }
     } else {
-      echo '  <tr>' . "\n" .
-       '    <td class="smallText" colspan="5">' . TEXT_NO_ORDER_HISTORY . '</td>' . "\n" .
-       '  </tr>' . "\n";
+?>
+  <tr>
+    <td class="smallText" colspan="5"><?= TEXT_NO_ORDER_HISTORY ?></td>
+  </tr>
+<?php
       }
 
 ?>
